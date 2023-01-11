8ba59338737361a3eec88a0b904c29683db8441d
john <king@mail.com>
2023-01-10 21:01:41 -0600

    issa coin

--- flip.py
+++ flip.py
@@ -16,27 +16,34 @@ longest_streak = ""
 streak_breaks = 0
 
 # Total count of heads and tails
-coin_flips_result = {"H": 0, "T": 0}
+heads_count = 0
+tails_count = 0
 
-# flips results
-flips_result = [ "H" if random.choice(["heads", "tails"]) == "heads" else "T" for i in range(n)]
+# list to store all the flips result
+flips_result = []
 
 # Flip a coin n times
-for coin in flips_result:
-    coin_flips_result[coin] +=1
+for i in range(n):
+    coin = random.choice(["heads", "tails"])
+    flips_result.append(coin[0])
+    # Check if the current flip is heads
+    if coin == "heads":
+        heads_count += 1
+    else:
+        tails_count += 1
+    # Check if the current flip continues the current streak
     if coin == current_streak:
         streak += 1
     else:
+        # Check if the current streak is longer than the previous longest streak
         if streak > streak_breaks:
             streak_breaks = streak
             longest_streak = current_streak
         streak = 1
         current_streak = coin
 
-heads_percentage = (coin_flips_result["H"]/n)*100
-tails_percentage = (coin_flips_result["T"]/n)*100
 print("Longest streak: ", longest_streak)
 print("Number of times streak is broken: ", streak_breaks)
-print(f"Heads count : {coin_flips_result['H']} ({heads_percentage:.2f}%) ")
-print(f"Tails count : {coin_flips_result['T']} ({tails_percentage:.2f}%) ")
+print(f"Heads count : {heads_count} ({(heads_count/n)*100:.2f}%) ")
+print(f"Tails count : {tails_count} ({(tails_count/n)*100:.2f}%) ")
 print("flips results: ", ''.join(flips_result))
